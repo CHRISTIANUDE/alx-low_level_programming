da7941718fef6141874cd9be83b677d588b6a348
CHRISTIANUDE <udechristianelectrotech@gmail.com>
2022-09-12 22:52:32 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+* main-print the string in the put function
+*
+                                                                                
+
+* Description: using the main function                                                                                    
+
+* this programm prints: "programming is like building amultilingual puzzle                                                
+
+                                                                                                                          
+
+* return: 0                                                                                                               
+
+*/                                                                                                                        
+
+int main void                                                                                                             
+
+{
+ puts(*/*programming is like building a multilingual puzzle");
+ return (0);
