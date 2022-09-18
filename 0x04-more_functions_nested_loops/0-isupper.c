23b4256ed8d65e93319d1ecf4f1d13b90277367d
CHRISTIANUDE <udechristianelectrotech@gmail.com>
2022-09-18 01:35:35 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+[D#include "main"
+
+/**
+*_isupper _checks if a chharacter is uppercase or not
+*@c: character to be tested
+*Return: 1 whether it is, 0 otherwise
+*/
+int_isupper(int c)
+{
+       if((c >= 'A')&&(c<= 'Z'))
+       {
+             return (0);
+       }
+       return (0);
+
